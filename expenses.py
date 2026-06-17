5d1ca80fae3957717893f9613275794d357ac293
Luis Colon <lccyber2024@gmail.com>
2026-06-16 20:05:55 -0400

    expenses

--- expenses.py
+++ expenses.py
@@ -1,5 +1,14 @@
-expenses = [10.50, 8.5, 15, 20, 5, 3 ]
-sum = 0 
-for x in expenses:
-    sum = sum + x
-print("You spent: $" + str(sum))
+#expenses = [10.50, 8.5, 15, 20, 5, 3 ]
+#sum = 0 
+#for x in expenses:
+#    sum = sum + x
+#print("You spent: $" + str(sum))
+
+total = 0 
+expenses = []
+
+for i in range(7):
+    expenses.append(float(input("Enter expense " + str(i + 1) + ": ")))
+
+    total = sum(expenses)
+print("You spent: $" + str(total))
